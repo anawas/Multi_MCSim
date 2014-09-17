77394913f4a80088e2e18d271f08c0f6ba12c4aa
Andreas Wassmer <aw@andreas-wassmer.ch>
2014-09-17 15:06:02 +0200

    [ADD] Added view and controller for virtual device.

---
+++ Multi_MCSim/VirtualMC.h
@@ -0,0 +1,13 @@
+//
+//  VirtualMC.h
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 17.09.14.
+//  Copyright (c) 2014 Phandroo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VirtualMC : NSViewController
+
+@end
---
+++ Multi_MCSim/VirtualMC.m
@@ -0,0 +1,26 @@
+//
+//  VirtualMC.m
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 17.09.14.
+//  Copyright (c) 2014 Phandroo. All rights reserved.
+//
+
+#import "VirtualMC.h"
+
+@interface VirtualMC ()
+
+@end
+
+@implementation VirtualMC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
