de9de2c731cd4420331e45f25dfdd2f9f1991b73
Andreas Wassmer <aw@andreas-wassmer.ch>
2014-09-18 18:20:26 +0200

    [REF] Renamed class VirtualMC to VMCConfigController which better reflects purpose of this class.

---
+++ Multi_MCSim/VMCConfigController.h
@@ -0,0 +1,13 @@
+//
+//  VMCConfigController.h
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 17.09.14.
+//  Copyright (c) 2014 Phandroo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VMCConfigController : NSViewController
+
+@end
