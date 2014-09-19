87be5548461426ee57d4e49176c6768da896e082
Andreas Wassmer <aw@andreas-wassmer.ch>
2014-09-19 08:03:41 +0200

    [ADD] Added class VirtualDevice that represents a device.

---
+++ Multi_MCSim/VirtualDevice.h
@@ -0,0 +1,36 @@
+//
+//  VirtualDevice.h
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 19.09.14.
+//  Copyright (c) 2014 Phandroo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+enum sensorType {
+    SENSOR_TEMPERATURE = 1,
+    SENSOR_HUMIDITY = (1 << 1),
+    SENSOR_LEVEL = (1 << 2),
+    SENSOR_STATUS = (1 << 3),
+    SENSOR_GEOLOCATION = (1 << 4),
+    SENSOR_MESSAGE = (1 << 5)
+};
+
+enum intervalMultiplier {
+    MULTIPLIER_SECONDS = 1,
+    MULTIPLIER_MINUTES = 60,
+    MULTIPLIER_HOURS = 3600
+};
+
+
+@interface VirtualDevice : NSObject
+@property (weak) NSString *deviceName;
+@property NSInteger updateInterval;
+@property NSInteger deviceNumber;
+@property BOOL deviceIsRunning;
+
+- (id)initWithDeviceName:(NSString *)devName andNumber:(NSInteger)devNumber;
+- (void)setUpdateInterval:(NSInteger)updateInterval withMutliplier:(NSInteger)multiplier;
+
+@end
---
+++ Multi_MCSim/VirtualDevice.m
@@ -0,0 +1,13 @@
+//
+//  VirtualDevice.m
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 19.09.14.
+//  Copyright (c) 2014 Phandroo. All rights reserved.
+//
+
+#import "VirtualDevice.h"
+
+@implementation VirtualDevice
+
+@end
