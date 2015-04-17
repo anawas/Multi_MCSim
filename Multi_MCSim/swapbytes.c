f168f6822744fcc8da7e71a509d10b692db0ab01
Andreas Wassmer <menkalinan56@gmail.com>
2015-04-17 10:56:34 +0200

    Added source file swapbytes.c/.h with helper functions swap_bytes_2() and swap_bytes_4().

---
+++ Multi_MCSim/swapbytes.c
@@ -0,0 +1,29 @@
+//
+//  byteswap.c
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 17.04.15.
+//  Copyright (c) 2015 Phandroo. All rights reserved.
+//
+
+#include "swapbytes.h"
+
+void swap_bytes_2(unsigned char *buffer) {
+    unsigned char temp;
+    
+    temp = buffer[0];
+    buffer[0] = buffer[1];
+    buffer[1] = temp;
+}
+
+void swap_bytes_4(unsigned char *buffer) {
+    unsigned char temp;
+    
+    temp = buffer[0];
+    buffer[0] = buffer[3];
+    buffer[3] = temp;
+    
+    temp = buffer[1];
+    buffer[1] = buffer[2];
+    buffer[2] = temp;
+};
---
+++ Multi_MCSim/swapbytes.h
@@ -0,0 +1,17 @@
+//
+//  byteswap.h
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 17.04.15.
+//  Copyright (c) 2015 Phandroo. All rights reserved.
+//
+
+#ifndef __Multi_MCSim__byteswap__
+#define __Multi_MCSim__byteswap__
+
+#include <stdio.h>
+
+void swap_bytes_2(unsigned char *buffer);
+void swap_bytes_4(unsigned char *buffer);
+
+#endif /* defined(__Multi_MCSim__byteswap__) */
