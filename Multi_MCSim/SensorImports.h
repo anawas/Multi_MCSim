a52772f46ef63ec7dac7d944d46aaaf2ad187e96
Andreas Wassmer <menkalinan56@gmail.com>
2015-04-17 10:57:19 +0200

    Added file SensorImports.h to collect all header imports.

---
+++ Multi_MCSim/SensorImports.h
@@ -0,0 +1,18 @@
+//
+//  SensorImports.h
+//  Multi_MCSim
+//
+//  Created by Andreas Wassmer on 16.04.15.
+//  Copyright (c) 2015 Phandroo. All rights reserved.
+//
+
+#ifndef Multi_MCSim_SensorImports_h
+#define Multi_MCSim_SensorImports_h
+
+#import "BatterySensor.h"
+#import "AccelerationSensor.h"
+#import "LocationSensor.h"
+#import "GSMSensor.h"
+#import "swapbytes.h"
+
+#endif
